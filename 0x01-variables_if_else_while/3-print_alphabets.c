bec563f47da9331ef977167ede868a9cac7df533
EsRuhie <estherruhi@gmail.com>
2023-07-14 07:11:22 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int lowerCase = 'a';
+    int upperCase = 'A';
+
+    while (lowerCase <= 'z')
+    {
+        putchar(lowerCase);
+        lowerCase++;
+    }
+
+    while (upperCase <= 'Z')
+    {
+        putchar(upperCase + 32);
+        upperCase++;
+    }
+
+    putchar('\n');
+    return (0);
+}
