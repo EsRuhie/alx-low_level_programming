811f4caeb3e93f9d0d7540139b89c55a950b606a
EsRuhie <estherruhi@gmail.com>
2023-07-14 07:57:57 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,11 @@
  */
 int main(void)
 {
-    int num = 0;
-
-    putchar(num + '0');
-    putchar('\n');
-
-    return (0);
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+return 0;
 }
