88312cda17eadd9ab50dc92d0d8e60eb806e339e
EsRuhie <estherruhi@gmail.com>
2023-07-16 14:32:37 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,24 @@
  */
 int main(void)
 {
-        int d, p, q;
+int d, p, q;
 
-        for (d = 0; d <= 7; d++)
-        {
-                for (p = d + 1; p <= 8; p++)
-                {
-                        for (q = p + 1; q <= 9; q++)
-                        {
-                                putchar(d + '0');
-                                putchar(p + '0');
-                                putchar(q + '0');
-                                if (d == 7 && p == 8 && q == 9)
-                                        continue;
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+for (d = 0; d <= 7; d++)
+{
+for (p = d + 1; p <= 8; p++)
+{
+for (q = p + 1; q <= 9; q++)
+{
+putchar(d + '0');
+putchar(p + '0');
+putchar(q + '0');
+if (d == 7 && p == 8 && q == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
