58a6085e50a875411dc8aa9082dbc234d7770345
EsRuhie <estherruhi@gmail.com>
2023-07-16 14:11:30 -0700

    TaSK 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int d, p, q;
+
+    for (d = 0; d <= 7; d++)
+    {
+        for (p = d + 1; p <= 8; p++)
+        {
+            for (q = p + 1; q <= 9; q++)
+            {
+                putchar(d + '0');
+                putchar(p + '0');
+                putchar(q + '0');
+
+                if (d == 7 && p == 8 && q == 9)
+                    continue;
+
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+    return 0;
+}
