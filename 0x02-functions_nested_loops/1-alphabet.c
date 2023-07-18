e3468bc3f618491ab1ac18d29008fbdeb412504d
EsRuhie <estherruhi@gmail.com>
2023-07-18 12:06:16 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    int i = 0;
+
+    while (alphabet[i] != '\0')
+    {
+        _putchar(alphabet[i]);
+        i++;
+    }
+
+    _putchar('\n');
+}
