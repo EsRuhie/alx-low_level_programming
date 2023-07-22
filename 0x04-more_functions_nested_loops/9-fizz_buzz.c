c1f2fda7f9420c7564c00b3899abde529b29dae4
EsRuhie <estherruhi@gmail.com>
2023-07-21 17:15:08 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,10 +9,10 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 int i;
+
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
@@ -22,7 +22,8 @@ printf("Fizz");
 else if (i % 5 == 0)
 printf("Buzz");
 else
-printf("%d ", i);
+printf("%d", i);
+
 if (i < 100)
 printf(" ");
 }
