9bd38c636e71c2927e016691ff8590995d4b85f0
EsRuhie <estherruhi@gmail.com>
2023-08-04 03:39:09 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -12,6 +12,7 @@ void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
+int main(void);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
