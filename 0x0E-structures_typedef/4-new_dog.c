bcddc5b7a042ffcea203ec60c358993b3838a7ed
EsRuhie <estherruhi@gmail.com>
2023-08-15 14:36:02 -0700

    Correction for Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,11 +8,15 @@
  */
 int _strlen(char *s)
 {
-int i = 0;
+int i;
+
+i = 0;
+
 while (s[i] != '\0')
 {
 i++;
 }
+
 return (i);
 }
 
@@ -26,16 +30,21 @@ return (i);
  */
 char *_strcpy(char *dest, char *src)
 {
-int len = 0;
+int len, i;
+
+len = 0;
+
 while (src[len] != '\0')
 {
 len++;
 }
-for (int i = 0; i < len; i++)
+
+for (i = 0; i < len; i++)
 {
 dest[i] = src[i];
 }
-dest[len] = '\0';
+dest[i] = '\0';
+
 return (dest);
 }
 
@@ -50,11 +59,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog;
 int len1, len2;
+
 len1 = _strlen(name);
 len2 = _strlen(owner);
+
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
+
 dog->name = malloc(sizeof(char) * (len1 + 1));
 if (dog->name == NULL)
 {
@@ -71,5 +83,6 @@ return (NULL);
 _strcpy(dog->name, name);
 _strcpy(dog->owner, owner);
 dog->age = age;
+
 return (dog);
 }
