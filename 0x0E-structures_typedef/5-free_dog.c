2427f3b441be78f366b37cdb5aca30947939225a
EsRuhie <estherruhi@gmail.com>
2023-08-15 14:25:18 -0700

    Correction for Task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,10 +2,13 @@
 	#include "dog.h"
 
 
-	/**
-	 * free_dog - frees memory allocated for a struct dog
-	 * @d: struct dog to free
-	 */
+/**
+ * free_dog - Frees memory allocated for a struct dog.
+ * @d: Pointer to the struct dog to free.
+ *
+ * This function frees the memory allocated for the fields of
+ * the struct dog, including its name and owner strings.
+ */
 	void free_dog(dog_t *d)
 	{
 		if (d)
